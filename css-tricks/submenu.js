f1936310e2e0aa154ca89ad969068b461bee51f5
VANDENHEEDE Driss <dvanden1@mobilebelgium.be>
2023-01-16 15:04:29 +0100

    rendre le site plus accessible

--- css-tricks/submenu.js
+++ css-tricks/submenu.js
@@ -1,6 +1,21 @@
+/*
 function toggleMenu() {
     var menu = document.querySelector('nav ul');
     menu.classList.toggle('show');
 }
-  
-  document.querySelector('.menu-toggle').addEventListener('click', toggleMenu);  
+
+document.querySelector('.menu-toggle').addEventListener('click', toggleMenu);
+*/
+let main = document.getElementById('main');
+let accordion_links = document.querySelectorAll('#accordion > li > a');
+console.log(accordion_links);
+accordion_links.forEach((link) => {
+  link.addEventListener('focus', () => {
+    let submenu = link.nextElementSibling;
+    main.style.marginTop = (submenu !== null) ? `${submenu.clientHeight}px` : 0;
+  });
+  link.addEventListener('mouseover', () => {
+    let submenu = link.nextElementSibling;
+    main.style.marginTop = (submenu !== null) ? `${submenu.clientHeight}px` : 0;
+  });
+});
