e845d25a097a12403d2bd1608ec74ca9a6d382cd
Driss Vandenheede <driss.vandenheede.techinfo@gmail.com>
2024-12-02 08:17:19 +0100

    modifications supplémentaires

--- pre.js
+++ pre.js
@@ -1,15 +1,27 @@
 const preElements = document.querySelectorAll('pre');
 
 preElements.forEach(preElement => {
-    preElement.addEventListener('click', () => {
-        const textToCopy = preElement.textContent;
+    preElement.setAttribute('tabindex', '0'); // Rendre focusable
+    preElement.setAttribute('role', 'button'); // Indiquer son rôle
+    preElement.setAttribute('aria-label', 'Copier le contenu dans le pressePapier');
 
-        navigator.clipboard.writeText(textToCopy)
-            .then(() => {
-                console.log('Texte copié dans le presse-papier');
-            })
-            .catch(err => {
-                console.error('Erreur pour copie du texte : ', err);
-            });
+    preElement.addEventListener('click', copyPreContent);
+    preElement.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            event.preventDefault();
+            copyPreContent(event);
+        }
     });
 });
+
+const copyPreContent = (event) => {
+    const textToCopy = event.target.textContent;
+
+    navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+            console.log('Texte copié dans le presse-papier');
+        })
+        .catch((err) => {
+            console.error('Erreur pour copie du texte : ', err);
+        });
+};
